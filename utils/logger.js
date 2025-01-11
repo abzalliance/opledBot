d3190f024a8c9c479bcc8930263f1c9a0ab3e126
xabz <117483033+abzalliance@users.noreply.github.com>
2025-01-11 17:52:42 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,42 +1,21 @@
-import chalk from 'chalk';
+// utils/logger.js
+import winston from 'winston';
 
-const logger = {
-    log: (level, message, value = '') => {
-        const now = new Date().toLocaleString();
-
-        const colors = {
-            info: chalk.green,
-            warn: chalk.yellow,
-            error: chalk.red,
-            success: chalk.blue,
-            debug: chalk.magenta,
-        };
-
-        const color = colors[level] || chalk.white;
-        const levelTag = `[ ${level.toUpperCase()} ]`;
-        const timestamp = `[ ${now} ]`;
-
-        const formattedMessage = `${chalk.green("[ OpenLedger ]")} ${chalk.cyanBright(timestamp)} ${color(levelTag)} ${message}`;
-
-        let formattedValue = ` ${chalk.green(value)}`;
-        if (level === 'error') {
-            formattedValue = ` ${chalk.red(value)}`;
-        } else if (level === 'warn') {
-            formattedValue = ` ${chalk.yellow(value)}`;
-        }
-        if (typeof value === 'object') {
-            const valueColor = level === 'error' ? chalk.red : chalk.green;
-            formattedValue = ` ${valueColor(JSON.stringify(value))}`;
-        }
-
-        console.log(`${formattedMessage}${formattedValue}`);
-    },
-
-    info: (message, value = '') => logger.log('info', message, value),
-    warn: (message, value = '') => logger.log('warn', message, value),
-    error: (message, value = '') => logger.log('error', message, value),
-    success: (message, value = '') => logger.log('success', message, value),
-    debug: (message, value = '') => logger.log('debug', message, value),
-};
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    // Відмітимо дату та час
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.printf(({ level, message, timestamp, ...meta }) => {
+      return `[${timestamp}] [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    // console
+    new winston.transports.Console()
+  ],
+});
 
 export default logger;
